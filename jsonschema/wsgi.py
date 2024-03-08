e50cec54e383bb00560b03b47089854de5d4fba5
Richard <richard@stromer.org>
2024-03-08 10:33:00 +0100

    run black

--- jsonschema/wsgi.py
+++ jsonschema/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jsonschema.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jsonschema.settings")
 
 application = get_wsgi_application()
